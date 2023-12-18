42fe45bc146e2c6f1cdabceb53a850646ef57ac7
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-12-18 23:55:02 +0500

    Base activity is created

---
+++ app/src/main/java/uz/jasurbekruzimov/deliveryapp/Activity/BaseActivity.java
@@ -0,0 +1,26 @@
+package uz.jasurbekruzimov.deliveryapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import uz.jasurbekruzimov.deliveryapp.R;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+
+    }
+}
